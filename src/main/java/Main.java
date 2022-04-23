079fde96a592736c4f5f13306658218fa2623dd4
Dima <dimkadruz@icloud.com>
2022-04-23 18:50:17 +0300

    Убрал статические методы.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
-        Class cl = TestWork.class;
+        Class<?> clazz = TestWork.class;
+        ClassTest classTest = new ClassTest();
 
         try {
-            ClassTest.start(cl);
-        } catch (InvocationTargetException | IllegalAccessException e) {
+            classTest.start(clazz);
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException | InstantiationException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/TestWork.java
+++ src/main/java/TestWork.java
@@ -1,37 +1,37 @@
 public class TestWork {
 
     @BeforeSuite
-    public static void testBefore() {
+    public void testBefore() {
         System.out.println("test before");
     }
 
     @Test(priority = 3)
-    public static void test1() {
+    public void test1() {
         System.out.println("test 1");
     }
 
     @Test(priority = 4)
-    public static void test2() {
+    public void test2() {
         System.out.println("test 2");
     }
 
     @Test(priority = 5)
-    public static void test3() {
+    public void test3() {
         System.out.println("test 3");
     }
 
     @Test(priority = 6)
-    public static void test4() {
+    public void test4() {
         System.out.println("test 4");
     }
 
     @Test(priority = 7)
-    public static void test5() {
+    public void test5() {
         System.out.println("test 5");
     }
 
     @AfterSuite
-    public static void testAfter() {
+    public void testAfter() {
         System.out.println("test after");
     }
 }
